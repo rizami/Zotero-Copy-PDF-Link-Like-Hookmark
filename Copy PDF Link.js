47e91a1dfc34af4e93b00d8e674bcfd633c28c8c
rizami <54391856+rizami@users.noreply.github.com>
2024-04-18 09:51:10 +0400

    Update Copy PDF Link.js

--- Copy PDF Link.js
+++ Copy PDF Link.js
@@ -2,7 +2,7 @@
 "translatorID":"70bf0b84-5070-4f3b-85a7-d7219eebbc74",
 "translatorType":2,
 "label":"Copy PDF Link",
-"creator":"Rizami Annuar, original using idea from Andy Verne",
+"creator":"Rizami Annuar, original using idea from Andy Verne, refined using ChatGPT-4",
 "target":"html",
 "minVersion":"1.0",
 "maxVersion":"",
@@ -13,9 +13,18 @@
 }
 
 function doExport() {
-	var item;
-	while(item = Zotero.nextItem()) {
-		Zotero.write("zotero://open-pdf/");
-		var library_id = item.libraryID ? item.libraryID : 0;
-		Zotero.write(library_id+"_"+item.key);
-	}}
+    var item;
+    var hasPDF = false;
+
+    while (item = Zotero.nextItem()) {
+        if (item.itemType === 'attachment' && item.contentType === 'application/pdf') {
+            Zotero.write("zotero://open-pdf/0_" + item.key);
+            hasPDF = true;
+        }
+    }
+
+    // If no PDF was found in the selection
+    if (!hasPDF) {
+        Zotero.write("No PDF file selected. Please select a PDF file to export its link.");
+    }
+}
